7db92c7685a4a09d560c38c2402e74ec2866bd08
Óscar Morales Vivó <oscarmv@mac.com>
2023-01-22 01:00:57 -0800

    Add project stationery.

---
+++ TakeHomeSimple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TakeHomeSimple
+//
+//  Created by Óscar Morales Vivó on 1/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
